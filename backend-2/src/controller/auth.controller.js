08c08651367d49919cea81e9a60093844263db3e
dakshjeet singh rathore <dakshjeet.12345@gmail.com>
2026-03-06 21:44:14 +0530

    redis added

--- backend-2/src/controller/auth.controller.js
+++ backend-2/src/controller/auth.controller.js
@@ -2,6 +2,7 @@ const userModel = require('../models/user.model')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const blacklistModel = require('../models/blacklist.model')
+const redis = require('../config/cache')
 
 async function registerUser(req,res){
       
@@ -104,15 +105,17 @@ async function logoutUser(req,res){
 
     res.clearCookie('token')
 
-    await blacklistModel.create({
-        token
-    })
+    await redis.set(token,Date.now().toString(),"EX",60*60)
 
     res.status(200).json({
         message: "logout sucessfully"
     })
 
 }
+
+/**
+ * key value
+ */
 
 
 
--- backend-2/src/middleware/auth.middleware.js
+++ backend-2/src/middleware/auth.middleware.js
@@ -1,3 +1,4 @@
+const redis = require('../config/cache')
 const blacklistModel = require('../models/blacklist.model')
 const userModel = require('../models/user.model')
 // const bcrypt = require('bcryptjs')
@@ -12,7 +13,7 @@ async function authUser(req,res,next){
         })
     }
 
-    const isTokenBlacklisted = await blacklistModel.findOne({token})
+    const isTokenBlacklisted = await redis.get(token)
 
     if(!isTokenBlacklisted){
         return res.status(401).json({
